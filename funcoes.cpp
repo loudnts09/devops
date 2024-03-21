f054109311e3c3935b068efff82efd5da9903179
Lourrayni Feliph <129124046+loudnts09@users.noreply.github.com>
2024-03-21 17:15:06 -0300

    commit do erro

---
+++ funcoes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "funcoes.h"
+
+int escolha;
+double valor;
+
+double celsiusparaFahrenheit(double temperatura){
+    double fahrenheit = (temperatura * 1.8) + 32;
+    std::cout << "A temperatura em Fahrenheit é: "<< fahrenheit;
+
+    return 0;
+}
+
+double fahrenheitparaCelsius(double temperatura){
+    double celsius = (temperatura - 32)/ 1.8;
+    std::cout << "A temperatura em Celsius é: " << celsius;
+
+    return 0;
+}
